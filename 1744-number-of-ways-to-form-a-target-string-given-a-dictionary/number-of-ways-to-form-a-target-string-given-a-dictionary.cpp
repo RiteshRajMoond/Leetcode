3f04406a512a1cb676bfecb26d4e7e56976a93fe
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-29 10:39:33 +0530

    Time: 68 ms (73.48%) | Memory: 58.2 MB (53.04%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    const int mod = 1e9+7;
-    vector<vector<int>> dp;
+    const int mod = 1e9 + 7;
     vector<vector<int>> counts;
+
     int numWays(vector<string>& words, string target) {
-        // 1st -> Index on target  // 2nd -> K
-        pre(words);
-        dp = vector<vector<int>> (target.size(), vector<int> (words[0].size(), -1));
-        return f(words, target, 0, 0);
-    }
-private:
-    int f(vector<string>& words, string& target, int i, int j) {
-        // base case
-        if(i == target.size()) return 1;
-        if(j >= words[0].size()) return 0;
+        int m = target.size();  
+        int n = words[0].size(); 
+        preprocess(words);
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int nt = f(words, target, i, j+1)%mod;
-
-        int t = 0;
-        int count = counts[target[i]-'a'][j];
-        if (count > 0) {
-            t = (1LL * count * f(words, target, i + 1, j + 1))%mod;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 1;
         }
 
-        return dp[i][j] = (nt+t)%mod;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // nt
+                dp[i][j] = dp[i][j - 1];
+
+                // t
+                int count = counts[target[i - 1] - 'a'][j - 1];
+                if (count > 0) {
+                    dp[i][j] = (dp[i][j] + 1LL * count * dp[i - 1][j - 1]) % mod;
+                }
+            }
+        }
+        return dp[m][n];
     }
 
-    void pre(vector<string>& words) {
+private:
+    void preprocess(vector<string>& words) {
         int m = words.size();
         int n = words[0].size();
 
-        counts = vector<vector<int>> (26, vector<int> (n, 0));
-
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < m; i++) {
+        counts = vector<vector<int>>(26, vector<int>(n, 0));
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
                 counts[words[i][j] - 'a'][j]++;
             }
         }
