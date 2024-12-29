7eacfb368e2fedbd142677cc0f042721904bce75
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-29 16:58:57 +0530

    Time: 13 ms (64.27%) | Memory: 16.6 MB (51.88%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distance) {
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto&i : edges) {
-            adj[i[0]].push_back({i[1], i[2]});
-            adj[i[1]].push_back({i[0], i[2]});
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // Initialize the distance matrix
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        
+        // Distance from each city to itself is 0
+        for (int i = 0; i < n; ++i) {
+            dist[i][i] = 0;
         }
-
-        auto dijkstra = [&](int src) -> int {   
-            vector<int> dis(n, INT_MAX);
-            priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-            pq.push({0, src});
-            dis[src] = 0;
-
-            while(!pq.empty()) {
-                auto tp = pq.top();
-                pq.pop();
-
-                int d = tp.first;
-                int node = tp.second;
-
-                for(auto &i : adj[node]) {
-                    int v = i.first;
-                    int wt = i.second;
-
-                    if(dis[v] > wt + d) {
-                        dis[v] = wt + d;
-                        pq.push({dis[v], v});
+        
+        // Fill initial distances from edges
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+        
+        // Floyd-Warshall Algorithm
+        for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX) {
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
             }
-
-            int city = 0;
-            for(int i = 0; i < n; i++) {
-                if(i != src && dis[i] <= distance) city++;
+        }
+        
+        // Count reachable cities for each city within the distance threshold
+        int minReachable = n;  // To store the minimum number of reachable cities
+        int city = -1;         // To store the city with the minimum reachable cities
+        
+        for (int i = 0; i < n; ++i) {
+            int reachable = 0;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && dist[i][j] <= distanceThreshold) {
+                    ++reachable;
+                }
             }
-            return city;
-        };
-
-        int mn = INT_MAX, res = -1;
-
-        for(int i = 0; i < n; i++) {
-            int cur = dijkstra(i);
-            if(cur < mn || (cur == mn && i > res)) {
-                mn = cur;
-                res = i;
+            if (reachable < minReachable || (reachable == minReachable && i > city)) {
+                minReachable = reachable;
+                city = i;
             }
         }
-
-        return res;
+        
+        return city;
     }
 };
