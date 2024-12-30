2d3268c7059374117bd1580f005c002f1965db44
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2024-12-30 11:01:08 +0530

    Time: 3 ms (96.09%) | Memory: 11.7 MB (62.82%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -2,14 +2,14 @@
 class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp (high+1, -1);
         const int mod = 1e9+7;
-        vector<int> dp(high+1, 0);
 
-        for (int sz = high; sz >= 0; --sz) {
+        for(int sz = high; sz >= 0; sz--) {
             int inc = (sz >= low && sz <= high) ? 1 : 0;
-            ll on = (sz + one <= high) ? dp[sz + one] : 0;
-            ll ze = (sz + zero <= high) ? dp[sz + zero] : 0;
-            dp[sz] = (on + ze + inc) % mod;
+            ll on = (sz + one <= high) ? dp[sz+one] : 0;
+            ll ze = (sz + zero <= high) ? dp[sz+zero] : 0;
+            dp[sz] = (on+ze+inc)%mod;
         }
 
         return dp[0];
