605486c8622b958a7829f1260fc24cd4042be14e
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-01-03 22:30:59 +0530

    Time: 610 ms (50.58%) | Memory: 183.9 MB (67.09%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,21 +1,73 @@
+class Node {
+private:
+    Node* links[2];
+public:
+    Node() {
+        links[0] = NULL;
+        links[1] = NULL;
+    }
+
+    bool containsKey(int bit) {
+        return links[bit] != NULL;
+    }
+
+    void put(int bit, Node* node) {
+        links[bit] = node;
+    }
+
+    Node* get(int bit) {
+        return links[bit];
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(int num) {
+        Node* node = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(!node->containsKey(bit)) {
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+
+    int get_max_xor(int num) {
+        int res = 0;
+        Node* node = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if(node->containsKey(!bit)) {
+                res |= (1 << i);
+                node = node->get(!bit);
+            } else {
+                node = node->get(bit);
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int max_xor = 0, mask = 0;
-        for (int i = 31; i >= 0; --i) {
-            mask |= (1 << i);
-            unordered_set<int> prefix_set;
-            for (int num : nums) {
-                prefix_set.insert(num & mask);
-            }
-            int new_xor = max_xor | (1 << i);
-            for (int prefix : prefix_set) {
-                if (prefix_set.count(new_xor ^ prefix)) {
-                    max_xor = new_xor;
-                    break;
-                }
-            }
+        int n = nums.size();
+        Trie t;
+        for(int i = 0; i < n; i++) {
+            t.insert(nums[i]);
         }
-        return max_xor;
+
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            res = max(t.get_max_xor(nums[i]), res);
+        }
+        return res;
     }
 };
