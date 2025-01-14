d7aa96e265ceeaddf7e6375777ef889ce0ee6b40
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-01-14 12:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (79.93%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,13 +4,14 @@ public:
         int n = nums.size();
         stack<int> st;
         vector<int> ans(n, -1);
-        
-        for(int i = 2*n-1; i >= 0; i--) {
-            while(!st.empty() && nums[st.top()] <= nums[i%n]) st.pop();
-            if(i < n && !st.empty()) ans[i] = nums[st.top()];
-            st.push(i%n);
-        }
 
+        for(int i = 0; i < 2*n; i++) {
+            while(!st.empty() && nums[st.top()] < nums[i%n]) {
+                ans[st.top()] = nums[i%n];
+                st.pop();
+            }
+            if(i < n) st.push(i);
+        }
         return ans;
     }
 };
