6121ac6850aac127e5f5241d2eb572087285442b
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-01-14 12:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (40.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -6,9 +6,9 @@ public:
         stack<int> st;
 
         for(int i = n2-1; i >= 0; i--) {
-            while(!st.empty() && nums2[i] > st.top()) st.pop();
-            if(st.empty()) mp[nums2[i]] = -1;
-            else mp[nums2[i]] = st.top();
+            while(!st.empty() && st.top() < nums2[i]) st.pop();
+            if(!st.empty()) mp[nums2[i]] = st.top();
+            else mp[nums2[i]] = -1;
             st.push(nums2[i]);
         }
 
