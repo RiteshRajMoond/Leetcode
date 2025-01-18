b949b1156ec8590ca07e1905962c639321544c43
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-01-18 11:37:33 +0530

    Time: 54 ms (20.01%) | Memory: 75.8 MB (8.99%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,23 +10,65 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        multiset<int> st;
-        ListNode* temp = head;
-        while(temp) {
-            st.insert(temp->val);
+    ListNode* sortList(ListNode* head) { return merge_sort(head); }
+
+private:
+    ListNode* merge_sort(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode* mid = middle(head);
+
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = nullptr;
+
+        left = merge_sort(left);
+        right = merge_sort(right);
+
+        ListNode* merged = merge(left, right);
+        return merged;
+    }
+
+    ListNode* middle(ListNode* head) {
+        ListNode *slow = head, *fast = head, *prev = nullptr;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return prev ? prev : head; 
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+
+        ListNode* t1 = l1;
+        ListNode* t2 = l2;
+        ListNode* ans = new ListNode(-1);
+        ListNode* temp = ans;
+
+        while (t1 && t2) {
+            if (t1->val < t2->val) {
+                temp->next = t1;
+                t1 = t1->next;
+            } else {
+                temp->next = t2;
+                t2 = t2->next;
+            }
             temp = temp->next;
         }
 
-        temp = head;
+        if (t1)
+            temp->next = t1;
+        else if (t2)
+            temp->next = t2;
 
-        while(temp) {
-            temp->val = *st.begin();
-            st.erase(st.begin());
-            temp = temp->next;
-        }
-
-        return head;
+        return ans->next;
     }
 };
