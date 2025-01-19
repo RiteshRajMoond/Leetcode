75bd8ddba6a645249d1561d0ab45e700b857d9ed
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-01-19 12:57:05 +0530

    Time: 68 ms (37.05%) | Memory: 21 MB (38.71%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& grid) {
-        // create a min-heap
         priority_queue< tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>> > pq;
         int m = grid.size();
         int n = grid[0].size();
 
-        // push the borders in
+        // push border in pq;
         for(int i = 0; i < n; i++) {
-            pq.push({grid[0][i], 0, i}); // ht, x, y
+            pq.push({grid[0][i], 0, i});
             pq.push({grid[m-1][i], m-1, i});
         }
 
@@ -17,29 +16,28 @@ public:
             pq.push({grid[i][n-1], i, n-1});
         }
 
-        int dx[] = {0, 1, 0, -1};
-        int dy[] = {1, 0, -1, 0};
-
         vector<vector<bool>> vis(m, vector<bool> (n, false));
 
         int res = 0;
         int max_h = -1;
 
-        // bfs
+        int dx[] = {0, 1, 0, -1};
+        int dy[] = {1, 0, -1, 0};
+
         while(!pq.empty()) {
             auto [ht, x, y] = pq.top(); pq.pop();
-            max_h = max(ht, max_h);
 
             if(vis[x][y]) continue;
             vis[x][y] = true;
 
-            res += (max_h - ht);
+            max_h = max(max_h, ht);
+            res += (max_h - grid[x][y]);
 
             for(int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(nx >= 0 && nx < m && ny >= 0 && ny < n && !vis[nx][ny]) {
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n && !vis[nx][ny]) {
                     pq.push({grid[nx][ny], nx, ny});
                 }
             }
@@ -48,3 +46,13 @@ public:
         return res;
     }
 };
+
+
+// 3 4 1 5 7
+// 3 4 1 5 7
+// 3 4 1 5 7
+// 3 4 1 5 7
+
+// bfs
+// min-heap
+// tuple -> ht, x, y
