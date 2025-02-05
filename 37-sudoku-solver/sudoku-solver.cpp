bc3b5b9b13d161c01d35142e1b3de0bf1b737487
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-05 12:21:25 +0530

    Time: 352 ms (56.41%) | Memory: 8.9 MB (28.94%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,21 +1,10 @@
 class Solution {
-    bool is_safe(vector<vector<char>>&board, int row, int col, char num) {
-        // check in row
-        // check in col
-        for(int j = 0; j < 9; j++) {
-            if(board[row][j] == num) return false;
-            if(board[j][col] == num) return false;
+    bool is_safe(vector<vector<char>>& board, int row, int col, char num) {
+        int s_row = (row/3)*3, s_col = (col/3)*3;
+        for(int i = 0; i < 9; i++) {
+            if(board[row][i] == num || board[i][col] == num || board[s_row + i/3][s_col + i%3] == num)
+                return false;
         }
-        
-        // check in the 3X3 box
-        int s_row = (row/3)*3;
-        int s_col = (col/3)*3;
-        for(int i = s_row; i < s_row+3; i++) {
-            for(int j = s_col; j < s_col+3; j++) {
-                if(board[i][j] == num) return false;
-            }
-        }
-
         return true;
     }
 
@@ -24,23 +13,19 @@ class Solution {
 
         int nxt_row = row, nxt_col = col+1;
         if(nxt_col == 9) {
-            nxt_col = 0;
             nxt_row = row+1;
+            nxt_col = 0;
         }
 
-        if(board[row][col] != '.') {
-            return solve_sudoku(board, nxt_row, nxt_col);
-        }
+        if(board[row][col] != '.') return solve_sudoku(board, nxt_row, nxt_col);
 
-        for(int i = 1; i <= 9; i++) {
-            char num = i + '0';
-            if(!is_safe(board, row, col, num)) continue;
-            board[row][col] = num;
-            bool res = solve_sudoku(board, nxt_row, nxt_col);
-            if(res) return true;
-            board[row][col] = '.';
+        for(char num = '1'; num <= '9'; num++) {
+            if(is_safe(board, row, col, num)) {
+                board[row][col] = num;
+                if(solve_sudoku(board, nxt_row, nxt_col)) return true;
+                board[row][col] = '.';
+            }
         }
-
         return false;
     }
 public:
