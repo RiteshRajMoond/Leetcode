b481319485d296158fa3a990a9f19b02de052f61
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-13 15:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (72.73%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int candies = 1;
-        int i = 1;
-
-        while(i < n) {
-            if(ratings[i] == ratings[i-1]) {
-                candies++;
+        int candies = 1, i = 1;
+        while (i < n) {
+            if (ratings[i - 1] == ratings[i]) {
                 i++;
+                candies++;
                 continue;
             }
 
-            // calculating peak
+            // ascent
             int peak = 1;
-            while(i < n && ratings[i] > ratings[i-1]) {
-                i++;
+            while (i < n && ratings[i] > ratings[i - 1]) {
                 peak++;
                 candies += peak;
+                i++;
             }
 
-            // calculating descent
+            // descent
             int down = 1;
-            while(i < n && ratings[i] < ratings[i-1]) {
-                i++;
+            while (i < n && ratings[i] < ratings[i - 1]) {
                 candies += down;
                 down++;
+                i++;
             }
 
-            if(down > peak) candies += (down - peak);
+            if (down > peak)
+                candies += (down - peak);
         }
-
-        return candies;        
+        return candies;
     }
 };
