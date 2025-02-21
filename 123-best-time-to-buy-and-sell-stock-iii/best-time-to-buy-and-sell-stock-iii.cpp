c6684a9e4d029b83f489fd2fc99a90c221245a9c
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-21 22:02:08 +0530

    Time: 27 ms (69.26%) | Memory: 79.4 MB (79.66%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,4 @@
 class Solution {
-    vector<vector<int>> dp;
-
-    // even -> buying, odd -> selling
-    int get_profit(vector<int>& prices, int idx, int num) {
-        if(idx == prices.size()) return 0;
-        if(num == 4) return 0;
-
-        if(dp[idx][num] != -1) return dp[idx][num];
-
-        if(num&1) {
-            int sell = get_profit(prices, idx+1, num+1) + prices[idx];
-            int not_sell = get_profit(prices, idx+1, num);
-
-            return dp[idx][num] = max(sell, not_sell);
-        } else {
-            int buy = get_profit(prices, idx+1, num+1) - prices[idx];
-            int not_buy = get_profit(prices, idx+1, num);
-
-            return dp[idx][num] = max(buy, not_buy);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
