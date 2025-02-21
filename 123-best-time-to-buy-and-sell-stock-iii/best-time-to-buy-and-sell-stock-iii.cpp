86edd7986fd5c16462a7dc0ca713337d4e0bcf7b
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-21 21:52:35 +0530

    Time: 171 ms (52.48%) | Memory: 136.9 MB (54.38%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,29 @@
 class Solution {
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
 
-    int get_profit(vector<int>& prices, int idx, int buy, int trans) {
+    // even -> buying, odd -> selling
+    int get_profit(vector<int>& prices, int idx, int num) {
         if(idx == prices.size()) return 0;
-        if(trans == 0) return 0;
+        if(num == 4) return 0;
 
-        if(dp[idx][buy][trans] != -1) return dp[idx][buy][trans];
+        if(dp[idx][num] != -1) return dp[idx][num];
 
-        // already bought we are gonna sell
-        if(buy) {
-            int sell = get_profit(prices, idx+1, 0, trans-1) + prices[idx];
-            int not_sell = get_profit(prices, idx+1, 1, trans);
-            return dp[idx][buy][trans] = max(sell, not_sell);
+        if(num&1) {
+            int sell = get_profit(prices, idx+1, num+1) + prices[idx];
+            int not_sell = get_profit(prices, idx+1, num);
+
+            return dp[idx][num] = max(sell, not_sell);
         } else {
-        // no stock. Need to buy it
-            int buys = get_profit(prices, idx+1, 1, trans) - prices[idx];
-            int not_buys = get_profit(prices, idx+1, 0, trans);
-            return dp[idx][buy][trans] = max(buys, not_buys);
+            int buy = get_profit(prices, idx+1, num+1) - prices[idx];
+            int not_buy = get_profit(prices, idx+1, num);
+
+            return dp[idx][num] = max(buy, not_buy);
         }
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        dp = vector<vector<vector<int>>> (n, vector<vector<int>> (2, vector<int> (3, -1)));
-        return get_profit(prices, 0, 0, 2);
+        dp = vector<vector<int>> (n, vector<int> (4, -1));
+        return get_profit(prices, 0, 0);
     }
 };
