95d7df7e72d7f4e430018c78bf62d49ef53ae8f1
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-22 17:31:23 +0530

    Time: 3 ms (62.80%) | Memory: 20.8 MB (77.02%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -12,39 +12,44 @@
 class Solution {
 public:
     TreeNode* recoverFromPreorder(string traversal) {
-        stack<TreeNode*> st;
-        int i = 0;
+        stack<TreeNode*> st; // maintains a path from root to node
         int n = traversal.size();
+        int i = 0;
 
         while(i < n) {
+            // calc level
             int lvl = 0;
             while(i < n && traversal[i] == '-') {
-                i++;
                 lvl++;
+                i++;
             }
 
+            // calc value
             int value = 0;
             while(i < n && isdigit(traversal[i])) {
                 value = value * 10 + traversal[i] - '0';
                 i++;
             }
 
+            // create node
             TreeNode* node = new TreeNode(value);
 
+            // go to the correct depth
             while(st.size() > lvl) st.pop();
 
             if(!st.empty()) {
                 if(!st.top()->left) {
                     st.top()->left = node;
-                } else {
+                } else 
                     st.top()->right = node;
-                }
             }
 
             st.push(node);
         }
 
+        // fetching root;
         while(st.size() != 1) st.pop();
+
         return st.top();
     }
 };
