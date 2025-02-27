bc2cf0eadbff1c36810225f92bfd1ecb42fdc52e
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-27 13:53:25 +0530

    Time: 155 ms (88.03%) | Memory: 150 MB (21.37%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
-        unordered_map<int, int> mp; 
-        vector<vector<int>> dp(n, vector<int>(n, 0)); // DP table
 
-        for(int i = 0; i < n; i++) {
-            mp[arr[i]] = i;
-        }
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++) mp[arr[i]] = i;
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        
+        int max_len = 0;
 
-        int max_length = 0;
-
-        // Loop through all pairs (i, j)
         for(int j = 1; j < n; j++) {
             for(int i = 0; i < j; i++) {
                 int target = arr[j] - arr[i];
-
-                // Only proceed if target < arr[i] to maintain strictly increasing order
                 if(target < arr[i] && mp.count(target)) {
-                    int k = mp[target]; // Get index of target
-                    
-                    // Transition Formula
-                    dp[i][j] = dp[k][i] + 1;
-
-                    // Every valid sequence has at least 3 elements
-                    max_length = max(max_length, dp[i][j] + 2);
+                    int k = mp[target];
+                    dp[i][j] = 1 + dp[k][i];    
+                    max_len = max(max_len, dp[i][j]+2);
                 }
             }
         }
 
-        return max_length >= 3 ? max_length : 0;
+        return max_len >= 3 ? max_len : 0;
     }
 };
