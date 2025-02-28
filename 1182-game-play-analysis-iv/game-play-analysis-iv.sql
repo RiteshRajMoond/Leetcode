b954cd8fd2cc306155d548278c9ed24500a374f1
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-02-28 13:45:34 +0530

    Time: 835 ms (79.41%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,17 +1,12 @@
-with firstlogin as (
-    select player_id, min(event_date) as first_login
+with first as (
+    select player_id, min(event_date) as f_login
     from activity
     group by player_id
 ),
-secondlogin as (
-    select distinct a.player_id
-    from activity a
-    join firstlogin f
-    on a.player_id = f.player_id
-    where DATEDIFF(a.event_date, f.first_login) = 1
+second as (
+    select f.player_id from first f
+    join activity a on f.player_id = a.player_id and datediff(a.event_date, f.f_login) = 1
 )
 
-select round(
-    (select count(player_id) from secondlogin) * 1.0 /
-    (select count(distinct player_id) from activity)
-    , 2) as fraction;
+select round((select count(player_id) from second) * 1.0/ (select count(distinct player_id) from activity),2) as fraction
+;
