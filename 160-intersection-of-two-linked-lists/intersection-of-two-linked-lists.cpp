52edb4ac9184ab9ca640f704f47a6f6308f72793
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-03-27 10:52:19 +0530

    Time: 29 ms (95.22%) | Memory: 18.5 MB (53.80%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -12,10 +12,9 @@ public:
         ListNode* a = headA, *b = headB;
 
         while(a != b) {
-            a = (!a) ? headB: a->next;
-            b = (!b) ? headA: b->next;
+            a = (!a) ? headB : a->next;
+            b = (!b) ? headA : b->next;
         }
-
         return a;
     }
 };
