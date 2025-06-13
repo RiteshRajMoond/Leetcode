98d04f04168ba4ecbd4ab216b95366be7e0af44f
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-06-13 22:29:02 +0530

    Time: 186 ms (5.24%) | Memory: 60.2 MB (19.55%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         if (nums.length < 2)
-            return Boolean.FALSE;
+            return false;
 
-        Set<Integer> set = new HashSet<>();
-
+        Map<Integer, Integer> freqMap = new TreeMap<>();
         for (int num : nums) {
-            set.add(num);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        return set.size() != nums.length;
+        for (int count : freqMap.values()) {
+            if (count > 1)
+                return true;
+        }
+        return false;
     }
 }
