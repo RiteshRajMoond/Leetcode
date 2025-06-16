a45f85f555c8f22e6867c2a0de050310ecbe2f29
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-06-16 16:25:56 +0530

    Time: 5 ms (29.94%) | Memory: 42.4 MB (22.18%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,14 +1,32 @@
-class Solution {
+import java.util.ArrayList;
+
+public class Solution {
     public int maximumDifference(int[] nums) {
         int n = nums.length;
-        int res = Integer.MIN_VALUE;
+        
+        ArrayList<Integer> minLeft = new ArrayList<>();
+        ArrayList<Integer> maxRight = new ArrayList<>();
 
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                int diff = nums[j] - nums[i];
-                res = Math.max(res, diff);
-            }
+            minLeft.add(0);
+            maxRight.add(0);
         }
-        return res <= 0 ? -1 : res;
+
+        minLeft.set(0, nums[0]);
+        for(int i = 1; i < n; i++) {
+            minLeft.set(i, Math.min(minLeft.get(i-1), nums[i]));
+        }
+
+        maxRight.set(n-1, nums[n-1]);
+        for(int i = n-2; i >= 0; i--) {
+            maxRight.set(i, Math.max(maxRight.get(i+1), nums[i]));
+        }
+
+        int maxDiff = 0;
+        for(int i = 0; i < n; i++) {
+            maxDiff = Math.max(maxDiff, maxRight.get(i) - minLeft.get(i));
+        }
+
+        return maxDiff > 0 ? maxDiff : -1;
     }
 }
